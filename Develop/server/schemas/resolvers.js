cd3fe46d88decc1db16314bdcd474bf5232af9e1
Craig <panamaworldwideinvestments@gmail.com>
2022-08-15 23:05:15 +0100

    refactored resolvers

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -1,19 +1,38 @@
-const { User} = require('../models');
+//  Resolvers Define the query and mutation functionality to work with the Mongoose models
+
+const {User, Book} = require('./models');
 
 const resolvers = {
-  Query: {
-    user: async () => {
-      return User.find({});
+    Query: {
+      Users:async () => {
+        return await User.find();
+        },
+        User: async (_, {id}) => {
+            return await User.findById(id);
+            },
+        Books: async () => {
+            return await Book.find();
+            }
     },
-   
-  },
-  Mutation: {
-    createUser: async (parent, args) => {
-      const user = await User.create(args);
-      return user;
+    Mutation: {
+        createUser: async (_, {input}) => {
+            const user = new User(input);
+            await user.save();
+            return user;
+            }
     },
+    User: {
+        savedBooks: async (user) => {
+            return await Book.find({
+                user: user._id
+            });
+        }
+    }
+}
+
+        
+
+
     
-  },
-};
 
 module.exports = resolvers;
--- Develop/server/utils/auth.js
+++ Develop/server/utils/auth.js
@@ -6,30 +6,30 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
-
-    // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+  authMiddleware: (req, res, next) => {
+    // check if there is a token in the header
+    const token = req.headers.authorization;
+    if (token) {
+      // verify token
+      jwt.verify(token, secret, (err, decoded) => {
+        if (err) {
+          res.status(401).json({
+            message: 'Invalid token',
+          });
+        } else {
+          // if token is valid, set the user to the decoded token
+          req.user = decoded;
+          next();
+        }
+      } );
+    } else {
+      res.status(401).json({
+        message: 'No token provided',
+      });
     }
 
-    if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
-    }
 
-    // verify token and get user data out of it
-    try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
-    }
-
-    // send to next endpoint
-    next();
+  
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
