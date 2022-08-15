afbe05cf498b382ab6625d578ad21d88cf54623f
Craig <panamaworldwideinvestments@gmail.com>
2022-08-15 22:05:01 +0100

    updatedtypedef.js

--- Develop/server/schemas/typedef.js
+++ Develop/server/schemas/typedef.js
@@ -1,22 +1,40 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type User {
-    _id: ID!
-    name: String!
-  }
-
- 
-
   type Query {
-    user: [User]
-    users(_id: String): [User]
-  }
+    users: [User!]!
+    me: User
+    user(id: ID!): User
 
-  type Mutation {
-    createUser(User: String!): User
-    
-  }
+
+    }
+    type Mutation {
+        login(email: String!, password: String!): AuthPayload!
+        addUser(name: String!):AuthPayload! 
+        saveBook(id: ID!): User
+        removeBook(id: ID!): User
+
+
+  type User {
+    id: ID!
+    username: String!
+    email: String!
+    bookCount: Int!
+    savedBooks: [Book!]!
+    }
+
+    type Book {
+        bookId: ID!
+        authors: String!
+        description: String!
+        title: String!
+        image: String!
+        link: String!
+    }
+    type AuthPayload {
+        user: User!
+        token: String!
+    }
 `;
 
 module.exports = typeDefs;
